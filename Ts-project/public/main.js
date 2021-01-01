2bca322b1e7a85719f1ba35b8fc9e06980f5c7cd
nurgnc <gencbedriyenur@gmail.com>
2021-01-01 17:37:56 +0300

    create project

---
+++ Ts-project/public/main.js
@@ -0,0 +1,56 @@
+"use strict";
+// const character = 'mario';
+// console.log(character);
+// const inputs = document.querySelectorAll('input');
+// console.log(inputs);
+// inputs.forEach(input => {
+//     console.log(input);
+// });
+// let character = 'mario';
+// let age = 30;
+// let isBlackBelt = false;
+// console.log('test');
+// let greet: Function;
+// greet = 'hello';
+// greet = () => {
+//     console.log('hello, again');
+// }
+// const add = (a : number, b: number, c: number | string = 10) => {
+//     console.log(a+b);
+//     console.log(c);
+// }
+// add(5, 10, 20);
+// const minus = (a: number, b: number) : number => {
+//     return a + b;
+// }
+// let result = minus(10, 7);
+// type StringOrNum = string | number;
+// type objWithName = { name: string, uid: StringOrNum};
+// const logDetails = (uid: StringOrNum, item: string) =>{
+//     console.log(`${item} has a uid of ${uid}`);
+// }
+// const greet = (user: objWithName) =>{
+//     console.log(`${user.name} says hello`);
+// }
+//ex 1
+// let greet: (a: string, b: string) => void;
+// greet = (name: string, greeting: string) => {
+//     console.log(`${name} says ${greeting}`);
+// }
+// //ex 2
+// let calc: (a: number, b: number, c: string) => number;
+// calc = (numOne: number, numTwo: number, action: string) => {
+//     if (action === 'add'){
+//         return numOne + numTwo;
+//     } else {
+//         return numOne - numTwo;
+//     }
+// }
+// //ex 3
+// let logDetails: (obj: {name: string, age: number}) => void;
+// type person = {name: string, age:number};
+// logDetails = (ninja: person) => {
+//     console.log(`${ninja.name} is ${ninja.age} years old`);
+// }
+// const anchor = document.querySelector('a')!;
+// console.log(anchor.href);
